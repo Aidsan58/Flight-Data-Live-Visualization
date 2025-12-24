3fdddd437e4e72e07d15dccd52ad7620db45fa17
Aidsan58 <aidanr1@comcast.net>
2025-12-23 17:48:04 -0800

    deleted:    flight4.py

--- flight4.py
+++
@@ -1,9 +0,0 @@
-from pykafka import KafkaClient
-
-# set up a Kafka client in Python by specifying the location of our Kafka broker
-client = KafkaClient(hosts="localhost:9092")
-
-# Connect newly spun consumer to flightdata topic
-# For each message in the topic, we print out the message payload
-for i in client.topics['flightdata'].get_simple_consumer():
-    print('data:{0}\n\n'.format(i.value.decode()))
